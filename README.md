b45144703167303ef8235afd839472e51823989c
coseo12 <coseo1221@gmail.com>
2024-07-09 15:49:48 +0900

    6-5 Uploading documents

--- README.md
+++ README.md
@@ -1854,13 +1854,94 @@ if message:
 
 위 코드의 실행 화면입니다.
 
-![Model Image](./images/6-4.png)
+![6-4 Image](./images/6-4.png)
 
 ## 6-5. Uploading Documents
 
-```py
+파일을 업로드하여 Stuff 방식으로 처리하는 코드를 작성해보겠습니다.
 
+이전에 환경설정 파일 및 캐시 폴더를 작성해 줍니다. (불필요 파일 제거)
+
+```toml
+# .streamlit/screts.toml
+OPENAI_API_KEY="sk-...n"
+
+# .cache/files
+# .cache/embeddings
 ```
+
+이제 실제 코드를 작성하겠습니다.
+
+```py
+import streamlit as st
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.storage import LocalFileStore
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.vectorstores import Chroma
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📜",
+)
+
+
+# 파일 처리
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    # cache_dir - 캐시 디렉토리
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    # chunk_size - 텍스트를 분할하는 크기
+    # chunk_overlap - 분할된 텍스트의 중복 크기
+    # separator - 텍스트를 분할하는 구분자
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=600,
+        chunk_overlap=100,
+        separator="\n",
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    # 캐시된 임베딩을 사용하여 Vector Store 초기화
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings,
+        cache_dir,
+    )
+    # Vector Store 초기화
+    vectorstore = Chroma.from_documents(docs, cached_embeddings)
+    retriver = vectorstore.as_retriever()
+    return retriver
+
+
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+Welcome!
+
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
+
+# 파일 업로드
+file = st.file_uploader("Upload a .txt .pdf or .docx file", type=["pdf", "txt", "docx"])
+
+if file:
+    s = embed_file(file)
+    docs = s.invoke("what is physics?")
+    st.write(docs)
+```
+
+실행결과 화면 입니다.
+
+![6-5 Image](./images/6-5.png)
 
 ## 6-6. Chat History
 
--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,34 +1,64 @@
 import streamlit as st
-import time
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.storage import LocalFileStore
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.vectorstores import Chroma
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📜",
 )
 
-# 메시지 저장소
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []    
 
-# 채팅 메시지 출력
-def send_message(message, role, save=True):
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+# 파일 처리
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
 
-# 캐싱한 채팅 메시지 출력
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
+    with open(file_path, "wb") as f:
+        f.write(file_content)
 
-# 채팅 입력
-message = st.chat_input("Send a message to AI")
+    # cache_dir - 캐시 디렉토리
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    # chunk_size - 텍스트를 분할하는 크기
+    # chunk_overlap - 분할된 텍스트의 중복 크기
+    # separator - 텍스트를 분할하는 구분자
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=600,
+        chunk_overlap=100,
+        separator="\n",
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    # 캐시된 임베딩을 사용하여 Vector Store 초기화
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embeddings,
+        cache_dir,
+    )
+    # Vector Store 초기화
+    vectorstore = Chroma.from_documents(docs, cached_embeddings)
+    retriver = vectorstore.as_retriever()
+    return retriver
 
-# 입력된 채팅 메시지 출력
-if message:
-    send_message(message, 'human')
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
 
-    with st.sidebar:
-        st.write(st.session_state)
+st.title("DocumentGPT")
+
+st.markdown(
+    """
+Welcome!
+
+Use this chatbot to ask questions to an AI about your files!
+"""
+)
+
+# 파일 업로드
+file = st.file_uploader("Upload a .txt .pdf or .docx file", type=["pdf", "txt", "docx"])
+
+if file:
+    s = embed_file(file)
+    docs = s.invoke("what is physics?")
+    st.write(docs)
