3477f6ba629ab4e1b6b6cb21a9a1d0aa32de1e23
coseo12 <coseo1221@gmail.com>
2024-07-12 15:38:51 +0900

    8-4 Formatter prompt

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -19,6 +19,175 @@ llm = ChatOpenAI(
 )
 
 
+# 문서 형식
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+        You are a helpfull assistant that is role playing as a teacher.
+         
+        Based ONLY on the following context make 10 questions to test the user's
+        knowledge about the text.
+
+        Each question should have 4 answers, three of them must be incorrect and
+        one should be correct.
+
+        Use (o) to signal the correct answer.
+
+        Question example:
+        
+        Question: What is the color of the ocean?
+        Answers: Red|Yellow|Green|Blue(o)
+        
+        Question: What is the capital or Georgia?
+        Answers: Bakum Tbilisi(o)|Manila|Beirut
+
+        Question: When was Avartar released?
+        Answers: 2007|2001|2009(o)|1998
+
+        Question: Who was Julius Caesar?
+        Answers: A Roamn Emperor(o)|Painter|Actor|Model
+
+        Your turn!
+
+        Context: {context}
+        """,
+        )
+    ]
+)
+
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a powerful formatting algorithm.
+     
+            You format exam questions into JSON format.
+            Answers with (o) are the correct ones.
+            
+            Example Input:
+            Question: What is the color of the ocean?
+            Answers: Red|Yellow|Green|Blue(o)
+                
+            Question: What is the capital or Georgia?
+            Answers: Baku|Tbilisi(o)|Manila|Beirut
+                
+            Question: When was Avatar released?
+            Answers: 2007|2001|2009(o)|1998
+                
+            Question: Who was Julius Caesar?
+            Answers: A Roman Emperor(o)|Painter|Actor|Model
+            
+            
+            Example Output:
+            
+            ```json
+            {{ "questions": [
+                    {{
+                        "question": "What is the color of the ocean?",
+                        "answers": [
+                                {{
+                                    "answer": "Red",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Yellow",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Green",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Blue",
+                                    "correct": true
+                                }},
+                        ]
+                    }},
+                                {{
+                        "question": "What is the capital or Georgia?",
+                        "answers": [
+                                {{
+                                    "answer": "Baku",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Tbilisi",
+                                    "correct": true
+                                }},
+                                {{
+                                    "answer": "Manila",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Beirut",
+                                    "correct": false
+                                }},
+                        ]
+                    }},
+                                {{
+                        "question": "When was Avatar released?",
+                        "answers": [
+                                {{
+                                    "answer": "2007",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "2001",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "2009",
+                                    "correct": true
+                                }},
+                                {{
+                                    "answer": "1998",
+                                    "correct": false
+                                }},
+                        ]
+                    }},
+                    {{
+                        "question": "Who was Julius Caesar?",
+                        "answers": [
+                                {{
+                                    "answer": "A Roman Emperor",
+                                    "correct": true
+                                }},
+                                {{
+                                    "answer": "Painter",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Actor",
+                                    "correct": false
+                                }},
+                                {{
+                                    "answer": "Model",
+                                    "correct": false
+                                }},
+                        ]
+                    }}
+                ]
+            }}
+            ```
+            Your turn!
+            Questions: {context}
+            """,
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
+
+
 # 파일 처리
 @st.cache_resource(
     show_spinner="Loading file...",
@@ -41,11 +210,6 @@ def split_file(file):
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
     return docs
-
-
-# 문서 형식
-def format_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
 
 
 st.title("QuizGPT")
@@ -82,46 +246,11 @@ if not docs:
     )
 else:
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
-        You are a helpfull assistant that is role playing as a teacher.
-         
-        Based ONLY on the following context make 10 questions to test the user's
-        knowledge about the text.
-
-        Each question should have 4 answers, three of them must be incorrect and
-        one should be correct.
-
-        Use (o) to signal the correct answer.
-
-        Question example:
-        
-        Question: What is the color of the ocean?
-        Answers: Red|Yellow|Green|Blue(o)
-        
-        Question: What is the capital or Georgia?
-        Answers: Bakum Tbilisi(o)|Manila|Beirut
-
-        Question: When was Avartar released?
-        Answers: 2007|2001|2009(o)|1998
-
-        Question: Who was Julius Caesar?
-        Answers: A Roamn Emperor(o)|Painter|Actor|Model
-
-        Your turn!
-
-        Context: {context}
-        """,
-            )
-        ]
-    )
-
-    chain = {"context": format_docs} | prompt | llm
-
     start = st.button("Generate Quiz")
 
     if start:
-        chain.invoke(docs)
+        questions_response = questions_chain.invoke(docs)
+        formatting_response = formatting_chain.invoke(
+            {"context": questions_response.content}
+        )
+        st.write(formatting_response.content)
